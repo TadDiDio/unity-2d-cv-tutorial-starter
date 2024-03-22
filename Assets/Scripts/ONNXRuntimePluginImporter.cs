b85284e2d6e92c3fc9b560a6157c91f86e99c5b1
Tad DiDio <taddidio@vt.edu>
2024-03-22 08:19:10 -0400

    Initial

---
+++ Assets/Scripts/ONNXRuntimePluginImporter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// Class with Native plugin functions for ONNX Runtime.
+/// </summary>
+public static class ONNXRuntimePluginImporter
+{
+    // Name of the DLL file
+    const string dll = "UnityONNXInferenceCVPlugin";
+
+    [DllImport(dll)]
+    public static extern int InitOrtAPI();
+
+    [DllImport(dll)]
+    public static extern int GetProviderCount();
+
+    [DllImport(dll)]
+    public static extern IntPtr GetProviderName(int index);
+
+    [DllImport(dll)]
+    public static extern void FreeResources();
+
+    [DllImport(dll)]
+    public static extern IntPtr LoadModel(string model, string execution_provider, int[] inputDims);
+
+    [DllImport(dll)]
+    public static extern void PerformInference(byte[] inputData, float[] outputArray, int length);
+}
